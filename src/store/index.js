7adacd8513dd9cb9ce7f6b1e2de92a28a337d60c
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2025-04-24 23:54:55 +0530

    Adding custom middlewares to Redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.log(`
+    TIME: ${new Date().toLocaleString()}
+    ACTION TYPE: ${action.type}
+    STATE: ${JSON.stringify(store.getState())}
+    -----------------
+  `)
+  next(action)
+}
+
 const store = configureStore({
   reducer: {
     user: userReducer
+  },
+  middleware: (getDefaultMiddleware) => {
+    return [...getDefaultMiddleware(), loggerMiddleware]
   }
 })
 
